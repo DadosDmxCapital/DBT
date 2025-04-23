e2e8166c3ae466d3f7a9d4a17b38ef3cca6fdca4
Henrique Soares | Analista <henrique.soares@dmxcapital.ad>
2025-04-23 13:06:25 -0300

    Adicionando coluna indice de operacoes

--- models/fato_detalhes_operacoes.sql
+++ models/fato_detalhes_operacoes.sql
@@ -1,8 +1,34 @@
-SELECT id, operacao_id, tipotitulo, cmc7, numero, documentosacado, numeronota, vencimento, valorface, nomesacado, classerisco, timestamp_extracao
+SELECT 
+    id,
+    operacao_id,
+    tipotitulo,
+    cmc7,
+    numero,
+    documentosacado,
+    numeronota,
+    vencimento,
+    valorface,
+    nomesacado,
+    classerisco,
+    timestamp_extracao,
+    'Sec' AS Empresa
 FROM {{ source('public', 'f_operacoes_detalhes') }}
 
 UNION ALL
 
-SELECT id, operacao_id, tipotitulo, cmc7, numero, documentosacado, numeronota, vencimento, valorface, nomesacado, classerisco, timestamp_extracao
+SELECT 
+     id,
+    operacao_id,
+    tipotitulo,
+    cmc7,
+    numero,
+    documentosacado,
+    numeronota,
+    vencimento,
+    valorface,
+    nomesacado,
+    classerisco,
+    timestamp_extracao,
+    'Sec' AS Empresa
 FROM {{source('public', 'f_operacoes_fidc_detalhes') }}
 
--- target/compiled/my_dbt_project/models/fato_detalhes_operacoes.sql
+++ target/compiled/my_dbt_project/models/fato_detalhes_operacoes.sql
@@ -1,7 +1,33 @@
-SELECT id, operacao_id, tipotitulo, cmc7, numero, documentosacado, numeronota, vencimento, valorface, nomesacado, classerisco, timestamp_extracao
+SELECT 
+    id,
+    operacao_id,
+    tipotitulo,
+    cmc7,
+    numero,
+    documentosacado,
+    numeronota,
+    vencimento,
+    valorface,
+    nomesacado,
+    classerisco,
+    timestamp_extracao,
+    'Sec' AS Empresa
 FROM "operacoes"."public"."f_operacoes_detalhes"
 
 UNION ALL
 
-SELECT id, operacao_id, tipotitulo, cmc7, numero, documentosacado, numeronota, vencimento, valorface, nomesacado, classerisco, timestamp_extracao
+SELECT 
+     id,
+    operacao_id,
+    tipotitulo,
+    cmc7,
+    numero,
+    documentosacado,
+    numeronota,
+    vencimento,
+    valorface,
+    nomesacado,
+    classerisco,
+    timestamp_extracao,
+    'Sec' AS Empresa
 FROM "operacoes"."public"."f_operacoes_fidc_detalhes"
