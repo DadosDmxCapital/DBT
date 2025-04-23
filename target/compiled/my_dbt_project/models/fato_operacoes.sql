7b638815cc98a59502ae0fb5f777bc4a5e778ca7
Henrique Soares | Analista <henrique.soares@dmxcapital.ad>
2025-04-23 12:55:57 -0300

    Atualizando views para manter valores das operacoes

--- target/compiled/my_dbt_project/models/fato_operacoes.sql
+++ target/compiled/my_dbt_project/models/fato_operacoes.sql
@@ -1,4 +1,8 @@
+WITH base AS (
+
 SELECT 
+
+    'Sec' AS Empresa, -- Adicionando a coluna Empresa com o valor 'Sec'
     cedente::VARCHAR(8000) AS CEDENTE,
     etapa::VARCHAR(8000) AS ETAPA,
     CAST(NULLIF(REGEXP_REPLACE(cpf_cnpj_cedente, '[^0-9]', '', 'g'), '') AS BIGINT) AS CPF_CNPJ_CEDENTE,
@@ -11,16 +15,18 @@ SELECT
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_pagto_operacao, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_PAGTO_OPERACAO,
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_saldo, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_SALDO,
     operacao::INT AS OPERACAO,
-    TO_DATE("data", 'DD-MM-YYYY') AS DATA,
+    TO_DATE("data", 'DD/MM/YYYY') AS DATA,
     operador::VARCHAR(8000) AS OPERADOR,
     captador::VARCHAR(8000) AS CAPTADOR,
-    TO_DATE(pagamento_operacao, 'DD-MM-YYYY') AS PAGAMENTO_OPERACAO,
+    TO_DATE(pagamento_operacao, 'DD/MM/YYYY') AS PAGAMENTO_OPERACAO,
     CAST(NULL AS VARCHAR(8000)) AS CONTA_PAGTO
 FROM "operacoes"."public"."f_operacoes"
 
 UNION ALL
 
 SELECT 
+
+    'fidc' AS Empresa, -- Adicionando a coluna Empresa com o valor 'fidc'
     cedente::VARCHAR(8000) AS CEDENTE,
     etapa::VARCHAR(8000) AS ETAPA,
     CAST(NULLIF(REGEXP_REPLACE(cpf_cnpj_cedente, '[^0-9]', '', 'g'), '') AS BIGINT) AS CPF_CNPJ_CEDENTE,
@@ -33,9 +39,17 @@ SELECT
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_pagto_operacao, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_PAGTO_OPERACAO,
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_saldo, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_SALDO,
      operacao::INT AS OPERACAO,
-    TO_DATE("data", 'DD-MM-YYYY') AS DATA,
+    TO_DATE("data", 'DD/MM/YYYY') AS DATA,
     operador::VARCHAR(8000) AS OPERADOR,
     captador::VARCHAR(8000) AS CAPTADOR,
-    TO_DATE(pagamento_operacao, 'DD-MM-YYYY') AS PAGAMENTO_OPERACAO,
+    TO_DATE(pagamento_operacao, 'DD/MM/YYYY') AS PAGAMENTO_OPERACAO,
     CAST(NULL AS VARCHAR(8000)) AS CONTA_PAGTO
 FROM "operacoes"."public"."f_operacoes_fidc"
+
+)
+
+SELECT
+
+    *,
+    ROW_NUMBER() OVER (ORDER BY DATA , EMPRESA , OPERACAO ) AS INDICE_OPERACAO
+    FROM base -- Adicionando a coluna id com valores aleatórios
--- target/run/my_dbt_project/models/fato_operacoes.sql
+++ target/run/my_dbt_project/models/fato_operacoes.sql
@@ -3,7 +3,11 @@
     
     
   as (
-    SELECT 
+    WITH base AS (
+
+SELECT 
+
+    'Sec' AS Empresa, -- Adicionando a coluna Empresa com o valor 'Sec'
     cedente::VARCHAR(8000) AS CEDENTE,
     etapa::VARCHAR(8000) AS ETAPA,
     CAST(NULLIF(REGEXP_REPLACE(cpf_cnpj_cedente, '[^0-9]', '', 'g'), '') AS BIGINT) AS CPF_CNPJ_CEDENTE,
@@ -16,16 +20,18 @@
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_pagto_operacao, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_PAGTO_OPERACAO,
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_saldo, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_SALDO,
     operacao::INT AS OPERACAO,
-    TO_DATE("data", 'DD-MM-YYYY') AS DATA,
+    TO_DATE("data", 'DD/MM/YYYY') AS DATA,
     operador::VARCHAR(8000) AS OPERADOR,
     captador::VARCHAR(8000) AS CAPTADOR,
-    TO_DATE(pagamento_operacao, 'DD-MM-YYYY') AS PAGAMENTO_OPERACAO,
+    TO_DATE(pagamento_operacao, 'DD/MM/YYYY') AS PAGAMENTO_OPERACAO,
     CAST(NULL AS VARCHAR(8000)) AS CONTA_PAGTO
 FROM "operacoes"."public"."f_operacoes"
 
 UNION ALL
 
 SELECT 
+
+    'fidc' AS Empresa, -- Adicionando a coluna Empresa com o valor 'fidc'
     cedente::VARCHAR(8000) AS CEDENTE,
     etapa::VARCHAR(8000) AS ETAPA,
     CAST(NULLIF(REGEXP_REPLACE(cpf_cnpj_cedente, '[^0-9]', '', 'g'), '') AS BIGINT) AS CPF_CNPJ_CEDENTE,
@@ -38,10 +44,18 @@ SELECT
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_pagto_operacao, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_PAGTO_OPERACAO,
     CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor_saldo, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR_SALDO,
      operacao::INT AS OPERACAO,
-    TO_DATE("data", 'DD-MM-YYYY') AS DATA,
+    TO_DATE("data", 'DD/MM/YYYY') AS DATA,
     operador::VARCHAR(8000) AS OPERADOR,
     captador::VARCHAR(8000) AS CAPTADOR,
-    TO_DATE(pagamento_operacao, 'DD-MM-YYYY') AS PAGAMENTO_OPERACAO,
+    TO_DATE(pagamento_operacao, 'DD/MM/YYYY') AS PAGAMENTO_OPERACAO,
     CAST(NULL AS VARCHAR(8000)) AS CONTA_PAGTO
 FROM "operacoes"."public"."f_operacoes_fidc"
+
+)
+
+SELECT
+
+    *,
+    ROW_NUMBER() OVER (ORDER BY DATA , EMPRESA , OPERACAO ) AS INDICE_OPERACAO
+    FROM base -- Adicionando a coluna id com valores aleatórios
   );
