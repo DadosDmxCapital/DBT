940e6cf7730c2c44de6d0aa374bf31ecf7ae4c79
Henrique Soares | Analista <henrique.soares@dmxcapital.ad>
2025-04-24 15:37:04 -0300

    Corrigindo identações e adicionando novas logica de dados

--- target/compiled/my_dbt_project/models/fato_controlemercadorias.sql
+++ target/compiled/my_dbt_project/models/fato_controlemercadorias.sql
@@ -1,6 +1,10 @@
 SELECT 
   NULLIF(numero_pedido, '')::VARCHAR(8000) AS operador,
-  NULLIF(data_pedido, '')::INT AS numero_operacao,
+
+  CASE 
+    WHEN data_pedido ~ '^\d+$' THEN data_pedido::INT
+    ELSE NULL
+  END AS numero_operacao,
 
   CASE 
     WHEN cliente ~ '^\d{2}/\d{2}/\d{4}$' THEN TO_DATE(cliente, 'DD/MM/YYYY')
@@ -8,7 +12,13 @@ SELECT
   END AS data_operacao,
 
   NULLIF(produto, '')::VARCHAR(8000) AS cedente,
-  CAST(NULLIF(REGEXP_REPLACE(quantidade, '[^0-9]', '', 'g'), '') AS BIGINT) AS cpf_sacado,
+
+  CASE 
+    WHEN REGEXP_REPLACE(quantidade, '[^0-9]', '', 'g') ~ '^\d+$' THEN 
+      CAST(REGEXP_REPLACE(quantidade, '[^0-9]', '', 'g') AS BIGINT)
+    ELSE NULL
+  END AS cpf_sacado,
+
   NULLIF(status, '')::VARCHAR(8000) AS status,
 
   CASE 
