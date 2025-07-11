f7af859f151d05d5c8e1d039061c6d0e9249384e
henriquesmeira <henriquesmeira12@gmail.com>
2025-07-11 12:51:24 -0300

    quitados fraude

---
+++ target/compiled/my_dbt_project/models/fato_quitadosfraude.sql
@@ -0,0 +1,37 @@
+SELECT id_titulo,
+       vencimento,
+       CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR,
+       TO_DATE(data_quitacao, 'DD/MM/YYYY') AS data_quitacao,
+       sacado, 
+       cpf_cnpj_sacado,
+       cedente,
+       cpf_cnpj_cedente,
+       setor,
+       banco_cobrador,
+       agencia_cobradora,
+       praca_pagamento,
+       localidade_sacado,
+       criticas,
+       timestamp_extracao,
+       'Sec' AS Empresa
+FROM "operacoes"."public"."f_titulosquitadosfraude"
+
+UNION ALL
+
+SELECT id_titulo,
+       vencimento,
+       CAST(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(valor, '[^0-9,]', '', 'g'), ',', '.', 'g'), '') AS DECIMAL(10,2)) AS VALOR,
+       TO_DATE(data_quitacao, 'DD/MM/YYYY') AS data_quitacao,
+       sacado,
+       cpf_cnpj_sacado,
+       cedente,
+       cpf_cnpj_cedente,
+       setor,
+       banco_cobrador,
+       agencia_cobradora,
+       praca_pagamento,
+       localidade_sacado,
+       criticas,
+       timestamp_extracao,
+       'Fidc' AS Empresa
+FROM "operacoes"."public"."f_titulosquitadosfraude_fidc"
